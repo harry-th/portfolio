696efa930ae2fdeb2e38c1b2e3c500b6e4b20f8a
harry-th <42222781+harry-th@users.noreply.github.com>
2023-08-21 19:14:06 -0400

    creds 2

--- src/app/api/google-search-console/route.js
+++ src/app/api/google-search-console/route.js
@@ -3,8 +3,9 @@ const searchConsole = require('@googleapis/searchconsole');
 const { format, subWeeks } = require('date-fns');
 
 export async function GET(request) {
+    let credentials = JSON.parse(process.env.keyfile)
     const auth = await searchConsole.auth.getClient({
-        credentials: JSON.parse(process.env.keyfile), // Update with the correct path to your JSON key file
+        credentials,
         scopes: ['https://www.googleapis.com/auth/webmasters.readonly'],
     });
 
